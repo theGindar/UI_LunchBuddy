c094729910f08049687352550eb8258c3aad429d
Marvin <marvin.goeckel@gmail.com>
2019-04-02 12:29:04 +0200

    muffin

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("test_start_gui.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("start_gui.fxml"));
 			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
