ee2132fffe58f56b9443a57a7852181fe5a1d248
kevin <kevin.h@mail.de>
2019-04-02 17:50:38 +0200

    Cooles Logo

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,27 +1,28 @@
 package application;
 
-
 import einstellungen.EinstellungenController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-
+import javafx.scene.image.Image;
 
 public class Main extends Application {
-	
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-		//	EinstellungenController.class.getClassLoader().getResource("Einstellungen.fxml");
+			// EinstellungenController.class.getClassLoader().getResource("Einstellungen.fxml");
 			Parent root = FXMLLoader.load(getClass().getResource("/start/Start_gui.fxml"));
 
-			Scene scene = new Scene(root,300,550);
+			Scene scene = new Scene(root, 300, 550);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch(Exception e) {
+			primaryStage.getIcons().add(new Image("/application/donut.png"));
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
