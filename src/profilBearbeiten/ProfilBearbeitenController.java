fbcc8afc5415589869d091c76ee41563067f840c
Marvin <marvin.goeckel@gmail.com>
2019-04-03 16:57:36 +0200

    muffin

--- src/profilBearbeiten/ProfilBearbeitenController.java
+++ src/profilBearbeiten/ProfilBearbeitenController.java
@@ -3,14 +3,18 @@ package profilBearbeiten;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 
+import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,15 +25,18 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 // ProfilBearbeiten.fmxl 
-public class ProfilBearbeitenController {
+public class ProfilBearbeitenController extends Application implements Initializable  {
 	
 	@FXML												
 	private TextField txtName;
@@ -41,8 +48,6 @@ public class ProfilBearbeitenController {
 	private TextArea txtBeschreibung;
 	@FXML
 	private DatePicker txtAlter;
-	@FXML
-	private ImageView Picture;
 	@FXML
 	private RadioButton ButtonPartnersuche;
 	@FXML
@@ -62,6 +67,17 @@ public class ProfilBearbeitenController {
 	private File filepath;
 	
 		
+	
+	//Kreis erzeugen und Bild darin speichern
+@Override
+	public void initialize (URL url, ResourceBundle rb) {
+		kreis.setStroke(Color.SEAGREEN);
+		Image im= new Image("/application/profile_icon.png",false);
+		kreis.setFill(new ImagePattern(im));
+		kreis.setEffect(new DropShadow(+25d, 0d, +2d, Color.DARKSEAGREEN));
+		
+	}
+
 	// Profil kann nur gepeichert werden, wenn alle Pflichtfelder aufgefüllt wurden
 		@FXML
 		public void initialize() {
@@ -96,7 +112,7 @@ public class ProfilBearbeitenController {
 			try {
 				BufferedImage bufferedImage = ImageIO.read(filepath);
 				Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-				Picture.setImage(image);
+				kreis.setFill(new ImagePattern(image));	
 				
 			}catch(IOException e) {
 				
@@ -134,6 +150,12 @@ public class ProfilBearbeitenController {
 		
 		window.setScene(ProfilBearbeitenScene);
 		window.show();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 	
 
--- src/profilErstellen/ProfilErstellenController.java
+++ src/profilErstellen/ProfilErstellenController.java
@@ -3,14 +3,19 @@ package profilErstellen;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.io.Serializable;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 
+import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -21,13 +26,17 @@ import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Circle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class ProfilErstellenController {
+public class ProfilErstellenController extends Application implements Initializable {
 
 	@FXML
 	private TextField txtErstellenName;
@@ -55,9 +64,22 @@ public class ProfilErstellenController {
 	private ToggleGroup partnerErstellen;
 	@FXML
 	private PasswordField PasswordErstellen;
+	@FXML
+	private Circle kreis;
 	
 	private FileChooser fileChooser;
 	private File filepath;
+
+	
+	//Kreis erzeugen und Bild darin speichern
+@Override
+	public void initialize (URL url, ResourceBundle rb) {
+		kreis.setStroke(Color.SEAGREEN);
+		Image im= new Image("/application/profile_icon.png",false);
+		kreis.setFill(new ImagePattern(im));
+		kreis.setEffect(new DropShadow(+25d, 0d, +2d, Color.DARKSEAGREEN));
+		
+	}
 
 	// Profil kann nur erstellt werden, wenn alle Pflichtfelder aufgefüllt wurden
 	@FXML
@@ -93,7 +115,7 @@ public class ProfilErstellenController {
 		try {
 			BufferedImage bufferedImage = ImageIO.read(filepath);
 			Image image = SwingFXUtils.toFXImage(bufferedImage, null);
-			PictureErstellen.setImage(image);
+			kreis.setFill(new ImagePattern(image));
 			
 		}catch(IOException e) {
 			
@@ -130,6 +152,12 @@ public class ProfilErstellenController {
 
 		window.setScene(ProfilErstellenScene);
 		window.show();
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
