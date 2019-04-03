db3a5b0c085da5fa772ce06f086c5502db37cfa0
kevin <kevin.h@mail.de>
2019-04-03 19:39:37 +0200

    Listview

--- src/chatlist/ControllerChatlist.java
+++ src/chatlist/ControllerChatlist.java
@@ -1,12 +1,17 @@
 package chatlist;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import chat.ChatListViewCell;
+import chat.ChatMessage;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -14,18 +19,37 @@ import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.stage.Stage;
 
-public class ControllerChatlist {
+public class ControllerChatlist implements Initializable{
+	
 
-    @FXML
-    private Button chatlistzurueckbtn;
 
-	@FXML
-	ListView<String> listviewchatauflistung = new ListView<>();
-	ObservableList<String> data = FXCollections.observableArrayList (
-			"Rüdiger",
-			"Hans",
-			"Gerdrud",
-			"Hildegard");
+	    @FXML
+	    private ListView<Student> listView;
+
+	    private ObservableList<Student> studentObservableList;
+
+	    public ControllerChatlist()  {
+
+	        studentObservableList = FXCollections.observableArrayList();
+	        System.out.println("wallah");
+	        //add some Students
+	        studentObservableList.addAll(
+	                new Student("John Doe"),
+	                new Student("Jane Doe"),
+	                new Student("Donte Dunigan")
+	        );
+
+
+	    }
+
+	    @Override
+	    public void initialize(URL location, ResourceBundle resources) {
+	    	System.out.println("ammmkkkkkkk");
+	        listView.setItems(studentObservableList);
+	        listView.setCellFactory(studentListView -> new StudentListViewCell());
+
+
+	    }
 	
 	/**
 	 * Wenn man in der Chatlist-Scene den Zurück-Button drückt und zum
@@ -41,5 +65,19 @@ public class ControllerChatlist {
 		window.setScene(SwipeScene);
 		window.show();
 	}
+	
+	
+	public void ZuEinemChat(ActionEvent event) throws IOException {
+
+		Parent ChatParent = FXMLLoader.load(getClass().getResource("/chat/ChatGUI.fxml"));
+		Scene ChatScene = new Scene(ChatParent);
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		window.setScene(ChatScene);
+		window.show();
+	}
+	
+	
 	
 }
---
+++ src/chatlist/StudentListViewCell.java
@@ -0,0 +1,56 @@
+package chatlist;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.layout.GridPane;
+
+import java.io.IOException;
+
+public class StudentListViewCell extends ListCell<Student> {
+
+    @FXML
+    private Label label1;
+
+    @FXML
+    private Label label2;
+
+    
+    @FXML
+    private GridPane gridPane;
+
+    private FXMLLoader mLLoader;
+
+    @Override
+    protected void updateItem(Student student, boolean empty) {
+        super.updateItem(student, empty);
+
+        if(empty || student == null) {
+
+            setText(null);
+            setGraphic(null);
+
+        } else {
+            if (mLLoader == null) {
+                mLLoader = new FXMLLoader(getClass().getResource("ListCell.fxml"));
+                mLLoader.setController(this);
+
+                try {
+                    mLLoader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+            
+            label2.setText(student.getName());
+
+            
+
+            setText(null);
+            setGraphic(gridPane);
+        }
+
+    }
+}
