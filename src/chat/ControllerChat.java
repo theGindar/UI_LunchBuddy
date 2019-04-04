588f4020830032a0cf2b34137e18d0942d5d01c0
kevin <kevin.h@mail.de>
2019-04-04 23:40:24 +0200

    Chat funktioniert

--- src/chat/ControllerChat.java
+++ src/chat/ControllerChat.java
@@ -23,46 +23,56 @@ public class ControllerChat implements Initializable {
 	private final Model model;
 	@FXML
 	private ListView<ChatMessage> chatListView;
-	
-    @FXML
-    private Button btnzurueckzuchatlist;
 
-    @FXML
-    private Button btnSend;
+	@FXML
+	private Button btnzurueckzuchatlist;
 
-    @FXML
-    private TextField textFeldChat;
-	
-	private ObservableList<ChatMessage> messageObservableList; 
-	
+	@FXML
+	private Button btnSend;
+
+	@FXML
+	private TextField textFeldChat;
+
+	private ObservableList<ChatMessage> messageObservableList;
+
 	public ControllerChat(Model model) {
 		this.model = model;
-		//chatListView.getItems().add("asdfasfda");
+		// chatListView.getItems().add("asdfasfda");
 		messageObservableList = FXCollections.observableArrayList();
 		messageObservableList.addAll(
-				new ChatMessage("Guten Tag", true),
+				new ChatMessage("Guten Tag", true), 
 				new ChatMessage(model.getName(), false));
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		chatListView.setItems(messageObservableList);
 		chatListView.setCellFactory(lv -> new ChatListViewCell());
-		
-	}
-	
-	/**
-	 * Wenn man in dem Chat den Zurück-Button drückt und zur 
-	 * Chatauflistung weitergeleitet wird
-	 **/
-	public void einstellungenZurueckZuChatliste(ActionEvent event) throws IOException {
 
-		Parent swipeParent = FXMLLoader.load(getClass().getResource("/swipeScreen/Swipe.fxml"));
-		Scene swipeScene = new Scene(swipeParent);
+	}
+
+	/**
+	 * Wenn man in dem Chat den Senden-Button drückt um die Nachricht abzuschicken
+	 **/
+	public void btnSendonclick(ActionEvent event) throws IOException {
+		messageObservableList.add(new ChatMessage(textFeldChat.getText(), true));
+		System.out.println(textFeldChat.getText());
+		textFeldChat.setText("");
+
+	}
+
+	/**
+	 * Wenn man in dem Chat den Zurück-Button drückt und zur Chatauflistung
+	 * weitergeleitet wird
+	 **/
+	public void zurueckZuChatliste(ActionEvent event) throws IOException {
+
+		Parent zurueckZurChatlisteParent = FXMLLoader.load(getClass().getResource("/swipeScreen/Swipe.fxml"));
+		Scene zurueckZurChatlisteScene = new Scene(zurueckZurChatlisteParent);
 
 		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
 
-		window.setScene(swipeScene);
+		window.setScene(zurueckZurChatlisteScene);
 		window.show();
 	}
 }
