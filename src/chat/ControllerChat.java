67fc39ec73ebcee090bd4bfffaa4eed9f3aa25b9
kevin <kevin.h@mail.de>
2019-04-04 20:12:46 +0200

    chat

--- src/chat/ControllerChat.java
+++ src/chat/ControllerChat.java
@@ -1,19 +1,37 @@
 package chat;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import model.Model;
 
 public class ControllerChat implements Initializable {
 	private final Model model;
 	@FXML
 	private ListView<ChatMessage> chatListView;
+	
+    @FXML
+    private Button btnzurueckzuchatlist;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    private TextField textFeldChat;
 	
 	private ObservableList<ChatMessage> messageObservableList; 
 	
@@ -31,5 +49,20 @@ public class ControllerChat implements Initializable {
 		chatListView.setItems(messageObservableList);
 		chatListView.setCellFactory(lv -> new ChatListViewCell());
 		
+	}
+	
+	/**
+	 * Wenn man in dem Chat den Zurück-Button drückt und zur 
+	 * Chatauflistung weitergeleitet wird
+	 **/
+	public void einstellungenZurueckZuChatliste(ActionEvent event) throws IOException {
+
+		Parent swipeParent = FXMLLoader.load(getClass().getResource("/swipeScreen/Swipe.fxml"));
+		Scene swipeScene = new Scene(swipeParent);
+
+		Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+		window.setScene(swipeScene);
+		window.show();
 	}
 }
