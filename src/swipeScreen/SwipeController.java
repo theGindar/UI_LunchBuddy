b68f04691d490199e60c1176a04ba30912021541
Marvin <marvin.goeckel@gmail.com>
2019-04-04 17:57:51 +0200

    Revert "."
    This reverts commit 86c4d762b9655c69203776927fe1b4e03c392fd9.

--- src/swipeScreen/SwipeController.java
+++ src/swipeScreen/SwipeController.java
@@ -54,7 +54,7 @@ public class SwipeController extends Application implements Initializable {
 	 * Wenn man auf der SwipeScene den Settings-Button drückt und zum
 	 * EinstellungsScreen weitergeleitet wird
 	 **/
-	public void zumEinstellungen(ActionEvent event) throws IOException {
+	public void ZumEinstellungen(ActionEvent event) throws IOException {
 
 		Parent BtnEinstellungenParent = FXMLLoader.load(getClass().getResource("/einstellungen/Einstellungen.fxml"));
 		Scene BtnEinstellungenScene = new Scene(BtnEinstellungenParent);
@@ -69,7 +69,7 @@ public class SwipeController extends Application implements Initializable {
 	 * Wenn man in der EinloggSceene den Zurück-Button drückt und zum
 	 * AnfangsScreen(Profil erstellen//anmelden) weitergeleitet wird
 	 **/
-	public void zumProfil(ActionEvent event) throws IOException {
+	public void ZumProfil(ActionEvent event) throws IOException {
 
 		Parent ZumProfilParent = FXMLLoader.load(getClass().getResource("/profil/Profil.fxml"));
 		Scene ZumProfilScene = new Scene(ZumProfilParent);
@@ -84,7 +84,7 @@ public class SwipeController extends Application implements Initializable {
 	 * Wenn man in der SwipeScene den Chats-Button drückt und zum ChatScreen
 	 * weitergeleitet wird
 	 **/
-	public void zumChat(ActionEvent event) throws IOException {
+	public void ZumChat(ActionEvent event) throws IOException {
 
 		Parent ZumChatParent = FXMLLoader.load(getClass().getResource("/chatlist/Chats_gui.fxml"));
 		Scene ZumChatScene = new Scene(ZumChatParent);
