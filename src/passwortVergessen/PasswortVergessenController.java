c3976f7eff9b886ecc35a6e7e853356427bb3b4d
kevin <kevin.h@mail.de>
2019-04-05 15:47:29 +0200

    Enter Taste funktioniert

--- src/passwortVergessen/PasswortVergessenController.java
+++ src/passwortVergessen/PasswortVergessenController.java
@@ -54,75 +54,97 @@ public class PasswortVergessenController {
 	private Label labelMail;
 	@FXML
 	private TextField txtMail;
-	
-	
-	//Mache ich ganz am Schluss. //Kevin Alles Klar CHEF
+
+	// Klick auf "Enter" Taste bei email bestätigung
 	@FXML
 	public void onEnter(ActionEvent ae) throws IOException {
 		System.out.println("Enter gedrückt");
-	}
-		
-	//Hier wird die eMail bestätigt
-		public void eMailBestaetigen(ActionEvent event) throws IOException {
-			String pattern = "[\\w\\.äöü-]+@[\\w\\.äöü-]+\\.(de|com|net|org)";
-			Pattern pt = Pattern.compile(pattern);
-			Matcher m = pt.matcher(txtMail.getText());
-			if (m.find()) {
-				
-				labelpw1.setVisible(true);
-				labelpw2.setVisible(true);
-				pw1.setVisible(true);
-				pw2.setVisible(true);
-				pw3.setVisible(true);
-				btnNeuesPwSpeichern.setVisible(true);
-				btnBestaetigen.setVisible(false);
-				txtMail.setVisible(false);
-							
-				btnNeuesPwSpeichern.setText("Speichern");
-				labelMail.setText("Altes Password eingeben:");
-				
-			} else {
-				PwFalsch.setText("Bitte eine korrekte E-Mail eingeben");
-				PwFalsch.setTextFill(Color.web("#FF0000"));
 
-			}
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+		eMailBestaetigen(ae);
 
-	// Man kann sich nur Anmelden, wenn man seine E-Mail und sein Password angegeben
-	// hat
-//	@FXML
-//	public void initialize() {
-//		this.btnNeuesPwSpeichern.disableProperty().bind(
-//				Bindings.or(this.pw2.textProperty().isEmpty(), this.pw3.textProperty().isEmpty()));
-//
-//	}
-//	
-	
+	}
+
+	// Klick auf "Enter" Taste bei Passwort Eingabe
+	@FXML
+	public void onEnterPW(ActionEvent ae) throws IOException {
+		System.out.println("Enter gedrückt");
+
+		pwSpeichernBtn(ae);
+
+	}
+
+	// Hier wird die eMail bestätigt
+	public void eMailBestaetigen(ActionEvent event) throws IOException {
+		String pattern = "[\\w\\.äöü-]+@[\\w\\.äöü-]+\\.(de|com|net|org)";
+		Pattern pt = Pattern.compile(pattern);
+		Matcher m = pt.matcher(txtMail.getText());
+		if (m.find()) {
+
+			labelpw1.setVisible(true);
+			labelpw2.setVisible(true);
+			pw1.setVisible(true);
+			pw2.setVisible(true);
+			pw3.setVisible(true);
+			btnNeuesPwSpeichern.setVisible(true);
+			btnBestaetigen.setVisible(false);
+			txtMail.setVisible(false);
+
+			btnNeuesPwSpeichern.setText("Speichern");
+			labelMail.setText("Altes Password eingeben:");
+
+			PwFalsch.setText(" ");
+
+		} else {
+			PwFalsch.setText("Bitte eine korrekte E-Mail eingeben");
+			PwFalsch.setTextFill(Color.web("#FF0000"));
+
+		}
+	}
+
 	/**
 	 * Wenn man in der EinloggSceene den Anmelden-Button drückt und zum
 	 * Haupt-,SwipeScreen weitergeleitet wird
 	 **/
 	public void pwSpeichernBtn(ActionEvent event) throws IOException {
-		
-		
+		if (pw1.getText().trim().isEmpty()) {
+			PwFalsch.setText("Bitte alle Felder ausfüllen!");
+			PwFalsch.setTextFill(Color.web("#FF0000"));
+		}
+
+		if (pw2.getText().trim().isEmpty()) {
+			PwFalsch.setText("Bitte alle Felder ausfüllen!");
+			PwFalsch.setTextFill(Color.web("#FF0000"));
+		}
+
+		if (pw3.getText().trim().isEmpty()) {
+			PwFalsch.setText("Bitte alle Felder ausfüllen!");
+			PwFalsch.setTextFill(Color.web("#FF0000"));
+		}
+
+		if (pw3.getText().equals(pw2.getText())) {
+			if (pw1.getText().trim().isEmpty()) {
+				PwFalsch.setText("Bitte alle Felder ausfüllen!");
+				PwFalsch.setTextFill(Color.web("#FF0000"));
+			} else {
+				if (pw2.getText().trim().isEmpty()) {
+					PwFalsch.setText("Bitte alle Felder ausfüllen!");
+					PwFalsch.setTextFill(Color.web("#FF0000"));
+				} else {
+					if (pw3.getText().trim().isEmpty()) {
+						PwFalsch.setText("Bitte alle Felder ausfüllen!");
+						PwFalsch.setTextFill(Color.web("#FF0000"));
+					} else {
+						System.out.println("Korrekt!");
+						zurueckZuLogin(event);
+					}
+				}
+			}
+		} else {
+			PwFalsch.setText("Neue Passwörter nicht identisch!");
+			PwFalsch.setTextFill(Color.web("#FF0000"));
+		}
 
 	}
-	
 
 	/**
 	 * Wenn man in der PasswordVergessenSceene den Zurück-Button drückt und zum
@@ -137,7 +159,6 @@ public class PasswortVergessenController {
 
 		window.setScene(zurueckZuLoginScene);
 		window.show();
-	
 
-}
+	}
 }
